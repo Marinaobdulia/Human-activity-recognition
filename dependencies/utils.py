9a1c1aaf7df04ee43640ee7556a1a6d136d796b5
Marinaobdulia <marina.o.moreno@gmail.com>
2021-02-17 11:55:42 +0100

    Definition of preprocessing step

--- dependencies/utils.py
+++ dependencies/utils.py
@@ -1,1 +1,32 @@
 # meter todas las funciones xra importarlas
+from scipy.io import loadmat, savemat
+import pandas as pd
+from scipy import stats
+
+def file2df(file):
+    data = loadmat(file)
+    n_people = len(data['database_training'])
+    
+    for i in range(n_people):
+        if i==0:
+            df = pd.DataFrame(data['database_training'][i][0].transpose(), columns=['acc_z', 'acc_XY', 'gy_x',
+                                                                                    'gy_y', 'gy_z'])
+            df['User']=len(data['database_training'][i][1][0])*[0]
+            df['Activity']=data['database_training'][i][1][0]
+            df['Timestamp']=range(len(data['database_training'][i][1][0]))
+        else:
+            df2 = pd.DataFrame(data['database_training'][i][0].transpose(), columns=['acc_z', 'acc_XY', 'gy_x','gy_y', 'gy_z'])
+            df2['User']=len(data['database_training'][i][1][0])*[i]
+            df2['Activity']=data['database_training'][i][1][0]
+            df2['Timestamp']=range(len(data['database_training'][i][1][0]))
+            df = df.append(df2, ignore_index=True)
+    return df
+
+def create_sequence(X, y, time_steps=1, step=1):
+    Xs, ys = [], []
+    for i in range(0, len(X) - time_steps, step):
+        v = X.iloc[i:(i + time_steps)].values
+        labels = y.iloc[i: i + time_steps]
+        Xs.append(v)
+        ys.append(stats.mode(labels)[0][0])
+    return np.array(Xs), np.array(ys).reshape(-1, 1)
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-import dependencies.utils
+import dependencies.utils as utils
+from sklearn.preprocessing import RobustScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-# def preprocessing():
-#  guardar el dataset que se haya usado para el split (csv)
+def preprocessing(file):
+    df = utils.file2df(file)
+    # create sequences
+    Xs, ys = utils.create_sequence(
+    df[['acc_z', 'acc_XY', 'gy_x','gy_y', 'gy_z']],
+    df.Activity,
+    TIME_STEPS=1,
+    STEP=10)
+    
+    # hot encoding
+    enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    enc = enc.fit(ys)
+    ys = enc.transform(ys)
+    
+    # split
+    X_train, X_test0, y_train, y_test0 = train_test_split()
+    X_test, X_val, y_test, y_val =  train_test_split()
+    # Save X_train, X_test, X_val
+
+    # scale
+    scaler = RobustScaler()
+    scaler = scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, X_val, y_train, y_test, y_val
 
 # def model_train
 # guardar los gráficos de ROC en carpeta output con la fecha --> guardar aquí tb output
@@ -11,7 +37,7 @@ import dependencies.utils
 
 if name == '__main__':
     #preprocessing
-
+    X_train, X_test, X_val, y_train, y_test, y_val = preprocessing(file)
     #model train
 
     #model test
