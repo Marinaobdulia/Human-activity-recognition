fb982e35241cef6c6f8880bcb87548b866f92613
Marinaobdulia <marina.o.moreno@gmail.com>
2021-03-15 22:17:24 +0100

    Fixed prediction file

--- prediction.py
+++ prediction.py
@@ -1,21 +1,47 @@
 import keras
 import numpy as np
 import sys
+import os.path
+import argparse
+import logging
 
-def load_dataset(dataset):
-    X_val = np.load(dataset[0])
-    y_val = np.load(dataset[1])
-    return X_val, y_val
 
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return str(arg)  # return filename string
 
 if __name__ == '__main__':
-    model_file = sys.argv[1]
-    model = keras.models.load_model(model_file)
+    logging.basicConfig(filename='prediction.log', filemode='w', level=logging.INFO,
+    format='%(asctime)-15s - %(levelname)s- %(name)s - %(message)s')
 
-    dataset = [sys.argv[2], sys.argv[3]]
-    X_val, y_val = load_dataset(dataset)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", dest="model", required=True,
+                    help="keras model", metavar="FILE",
+                    type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-x", dest="x", required=True,
+                help="data matrix (X)", metavar="FILE",
+                type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-y", dest="y", required=False,
+                help="data classification", metavar="FILE",
+                type=lambda x: is_valid_file(parser, x))
+    args = parser.parse_args()
 
-    y_val_calculated = model.predict(X_val)
-    loss, acc = model.evaluate(X_val, y_val)
-    # print --> log (módulo logging)
-    
+    model = keras.models.load_model(args.model)
+
+    if args.y is not None:
+        X_val = np.load(args.x)
+        y_val = np.load(args.y)
+        loss, acc = model.evaluate(X_val, y_val)
+        logging.info(f'Model evaluation results: {acc*100:.2f}% acc {loss*100:.2f}% loss')
+    else:
+        X_pred = np.load(args.x)
+        y_pred = model.predict(X_pred)
+        np.save('predictions', y_pred)
+        logging.info(f'Prediction results are available on this folder')
+
+
+
+
+   
